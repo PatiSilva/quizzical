e760cc3d1d3cde2628cb1f0fc3e4776c03d77a07
MakerSquare Student <student@lab-2.att.net>
2013-07-01 11:09:48 -0500

    practiced javascript class exercise

--- quiz.js
+++ quiz.js
@@ -1,2 +1,23 @@
-document.write('<p>If you can see this, your quiz script is set up!</p>');
-console.log("Hello, welcome to Quizzical");
+//document.write('<p>If you can see this, your quiz script is set up!</p>');
+//console.log("Hello, welcome to Quizzical");
+var name = prompt ("Name?");
+    email = prompt ("Email?");
+    budget_in_dollars = prompt ("Budget?");
+    timeline_in_months = prompt ("Timeline?");
+
+budget_in_dollars = Number(budget_in_dollars);
+timeline_in_months = Number(timeline_in_months);
+
+monthly_budget = 0;
+
+incomes = [];
+
+while(income = prompt("Source?")) {
+  incomes.push(income);
+}
+
+for(i=0; i<incomes.length; i++) {
+  console.log(incomes[i]);
+}
+
+
